1e56b6c31c679df4a77d6c3a1102daf98ba5d0ca
FullofMyself <wmaseko1@live.com>
2025-06-19 18:18:59 +0200

    No Popup  Pictures on uniquescrubz website

--- src/pages/Manufacturing.jsx
+++ src/pages/Manufacturing.jsx
@@ -119,7 +119,7 @@ export default function Manufacturing() {
                   pauseOnHover: true,
                   draggable: true,
                   progress: undefined,
-                  theme: "colored",
+                  theme: "black",
                 });
               }}
             />
